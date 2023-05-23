a2fd91a91ccbfb3f9ded27a75f0f928b5ac63600
simrett <leesim242@gmail.com>
2023-05-23 17:10:18 -0300

    pathlink

---
+++ pathlink.c
@@ -0,0 +1,94 @@
+#include "shell.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to pointer to our linked list
+ * @str: pointer to string in previous first node
+ * Return: address of the new element/node
+ */
+
+list_path *add_node_end(list_path **head, char *str)
+{
+
+	list_path *tmp;
+	list_path *new;
+
+	new = malloc(sizeof(list_path));
+
+	if (!new || !str)
+	{
+		return (NULL);
+	}
+
+	new->dir = str;
+
+	new->p = '\0';
+	if (!*head)
+	{
+		*head = new;
+	}
+	else
+	{
+		tmp = *head;
+
+		while (tmp->p)
+		{
+
+			tmp = tmp->p;
+		}
+
+		tmp->p = new;
+	}
+
+	return (*head);
+}
+
+
+/**
+ * link_path - creates a linked list for path directories
+ * @path: string of path value
+ * Return: pointer to the created linked list
+ */
+list_path *link_path(char *path)
+{
+	list_path *head = '\0';
+	char *token;
+	char *c_path = _str_dup(path);
+
+	token = strtok(c_path, ":");
+	while (token)
+	{
+		head = add_node_end(&head, token);
+		token = strtok(NULL, ":");
+	}
+
+	return (head);
+}
+
+/**
+ * _where - finds the pathname of a filename
+ * @filename: name of file or command
+ * @head: head of linked list of path directories
+ * Return: pathname of filename or NULL if no match
+ */
+char *_where(char *filename, list_path *head)
+{
+	struct stat st;
+	char *string;
+
+	list_path *tmp = head;
+
+	while (tmp)
+	{
+
+		string = _str_cnct(tmp->dir, "/", filename);
+		if (stat(string, &st) == 0)
+		{
+			return (string);
+		}
+		free(string);
+		tmp = tmp->p;
+	}
+
+	return (NULL);
+}
