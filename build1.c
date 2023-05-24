715b4b053858562bd3cf31bc06de9df274b5f57c
zakisu250 <zakisu250@gmail.com>
2023-05-24 18:09:10 +0300

    fixed the add node funtion

--- build1.c
+++ build1.c
@@ -7,7 +7,7 @@
  */
 int pnt_lnk_lst(inf_o *inf)
 {
-	p_list(inf->hist);
+	pnt_list_struct(inf->hist);
 	return (0);
 }
 
@@ -22,13 +22,13 @@ int rm_aliases(inf_o *inf, char *s)
 	char *p, i;
 	int t;
 
-	p = _strchr(s, '=');
+	p = str_srch(s, '=');
 	if (!p)
 		return (1);
 	i = *p;
 	*p = 0;
-	t = delete_node_at_i(&(inf->alias),
-		get_node_i(inf->alias, node_starts_with(inf->alias, s, -1)));
+	t = node_delete(&(inf->alias),
+		get_node(inf->alias, prenode(inf->alias, s, -1)));
 	*p = i;
 	return (t);
 }
@@ -43,14 +43,14 @@ int create_aliases(inf_o *inf, char *s)
 {
 	char *p;
 
-	p = _strchr(s, '=');
+	p = str_srch(s, '=');
 	if (!p)
 		return (1);
 	if (!*++p)
 		return (rm_aliases(inf, s));
 
 	re_aliases(inf, s);
-	return (add_node_end(&(inf->alias), s, 0) == NULL);
+	return (add_beg_list(&(inf->alias), s, 0) == NULL);
 }
 
 /**
@@ -64,7 +64,7 @@ int pnt_alias(lists_t *n)
 
 	if (n)
 	{
-		p = _strchr(n->str, '=');
+		p = str_srch(n->str, '=');
 		for (s = n->str; s <= p; s++)
 			_putchar(*s);
 		_putchar('\'');
@@ -98,11 +98,11 @@ int mng_alias(inf_o *inf)
 	}
 	for (j = 1; inf->argv[j]; j++)
 	{
-		p = _strchr(inf->argv[j], '=');
+		p = str_srch(inf->argv[j], '=');
 		if (p)
 			create_aliases(inf, inf->argv[j]);
 		else
-			pnt_alias(node_starts_with(inf->alias, inf->argv[j], '='));
+			pnt_alias(prenode(inf->alias, inf->argv[j], '='));
 	}
 
 	return (0);
