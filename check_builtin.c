ca4821188c2155b82445112bb1502515a0d19e3c
simrett <leesim242@gmail.com>
2023-05-25 15:15:18 +0300

    builtin checker

---
+++ check_builtin.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ * p_env - prints the current_environnement
+ * @cmd: command entered
+ *
+ * Return: void
+ */
+
+void penv(char **cmd __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		print(environ[i], STDOUT_FILENO);
+		print("\n", STDOUT_FILENO);
+	}
+}
+
+/**
+ * eexit - exits the shell
+ * @cmd: command entered
+ *
+ * Return: void
+ */
+
+voide exit(char **cmd)
+{
+	int token = 0, arg;
+
+	for (; tokenized_command[token] != NULL; token++)
+		;
+	if (token == 1)
+	{
+		free(cmd);
+		free(line);
+		free(commands);
+		exit(status);
+	}
+	else if (token == 2)
+	{
+		arg = _atoi(cmd[1]);
+		if (arg == -1)
+		{
+			print(shell_name, STDERR_FILENO);
+			print(": 1: exit: Illegal number: ", STDERR_FILENO);
+			print(cmd[1], STDERR_FILENO);
+			print("\n", STDERR_FILENO);
+			status = 2;
+		}
+		else
+		{
+			free(line);
+			free(cmd);
+			free(commands);
+			exit(arg);
+		}
+	}
+	else
+		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+}
