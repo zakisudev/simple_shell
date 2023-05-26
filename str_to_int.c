08f935777ccff606d97e296edffaa87dd7f49207
zakisu250 <zakisu250@gmail.com>
2023-05-26 12:16:59 +0300

    spelling correction

--- str_to_int.c
+++ str_to_int.c
@@ -43,26 +43,27 @@ int _isalpha(int c)
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
-int _atoi(char *s)
+int _atoi(char *str)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	int i, sign = 1, flag = 0, result;
+	unsigned int num = 0;
+
+	for (i = 0; str[i] != '\0' && flag != 2; i++)
 	{
-		if (s[i] == '-')
+		if (str[i] == '-')
 			sign *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
+		if (str[i] >= '0' && str[i] <= '9')
 		{
 			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			num *= 10;
+			num += (str[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
 	}
 	if (sign == -1)
-		output = -result;
+		result = -num;
 	else
-		output = result;
-	return (output);
+		result = num;
+	return (result);
 }
